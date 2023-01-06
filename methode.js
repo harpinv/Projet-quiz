40d93a88e16a137181a523bdeea97eb6f8605d85
vince <vincent.harpin@orange.fr>
2023-01-06 21:10:32 +0100

    projet quiz

---
+++ methode.js
@@ -0,0 +1,354 @@
+let vraie = 0;
+let faux = 0;
+
+$('#commencer').click(function () {
+    $('.bloc1').css('display', 'block');
+    $(this).css('display', 'none');
+})
+
+$('#choixA1').click(function () {
+    $('.bloc2').css('display', 'block');
+    $('#masque1').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixA2').click(function () {
+    $('.bloc2').css('display', 'block');
+    $('#masque1').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    vraie += 1;
+})
+
+$('#choixB1').click(function () {
+    $('.bloc3').css('display', 'block');
+    $('#masque2').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    vraie += 1;
+})
+
+$('#choixB2').click(function () {
+    $('.bloc3').css('display', 'block');
+    $('#masque2').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixB3').click(function () {
+    $('.bloc3').css('display', 'block');
+    $('#masque2').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixB4').click(function () {
+    $('.bloc3').css('display', 'block');
+    $('#masque2').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixC1').click(function () {
+    $('.bloc4').css('display', 'block');
+    $('#masque3').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixC2').click(function () {
+    $('.bloc4').css('display', 'block');
+    $('#masque3').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixC3').click(function () {
+    $('.bloc4').css('display', 'block');
+    $('#masque3').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    vraie += 1;
+})
+
+$('#choixC4').click(function () {
+    $('.bloc4').css('display', 'block');
+    $('#masque3').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixD1').click(function () {
+    $('.bloc5').css('display', 'block');
+    $('#masque4').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixD2').click(function () {
+    $('.bloc5').css('display', 'block');
+    $('#masque4').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    vraie += 1;
+})
+
+$('#choixD3').click(function () {
+    $('.bloc5').css('display', 'block');
+    $('#masque4').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixD4').click(function () {
+    $('.bloc5').css('display', 'block');
+    $('#masque4').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixE1').click(function () {
+    $('.bloc6').css('display', 'block');
+    $('#masque5').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    vraie += 1;
+})
+
+$('#choixE2').click(function () {
+    $('.bloc6').css('display', 'block');
+    $('#masque5').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixE3').click(function () {
+    $('.bloc6').css('display', 'block');
+    $('#masque5').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixE4').click(function () {
+    $('.bloc6').css('display', 'block');
+    $('#masque5').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixF1').click(function () {
+    $('.bloc7').css('display', 'block');
+    $('#masque6').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixF2').click(function () {
+    $('.bloc7').css('display', 'block');
+    $('#masque6').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixF3').click(function () {
+    $('.bloc7').css('display', 'block');
+    $('#masque6').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixF4').click(function () {
+    $('.bloc7').css('display', 'block');
+    $('#masque6').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    vraie += 1;
+})
+
+$('#choixG1').click(function () {
+    $('.bloc8').css('display', 'block');
+    $('#masque7').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    vraie += 1;
+})
+
+$('#choixG2').click(function () {
+    $('.bloc8').css('display', 'block');
+    $('#masque7').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixG3').click(function () {
+    $('.bloc8').css('display', 'block');
+    $('#masque7').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixG4').click(function () {
+    $('.bloc8').css('display', 'block');
+    $('#masque7').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixH1').click(function () {
+    $('.bloc9').css('display', 'block');
+    $('#masque8').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixH2').click(function () {
+    $('.bloc9').css('display', 'block');
+    $('#masque8').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixH3').click(function () {
+    $('.bloc9').css('display', 'block');
+    $('#masque8').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixH4').click(function () {
+    $('.bloc9').css('display', 'block');
+    $('#masque8').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    vraie += 1;
+})
+
+$('#choixI1').click(function () {
+    $('.bloc10').css('display', 'block');
+    $('#masque9').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    vraie += 1;
+})
+
+$('#choixI2').click(function () {
+    $('.bloc10').css('display', 'block');
+    $('#masque9').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixI3').click(function () {
+    $('.bloc10').css('display', 'block');
+    $('#masque9').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixI4').click(function () {
+    $('.bloc10').css('display', 'block');
+    $('#masque9').css('display', 'block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+})
+
+$('#choixJ1').click(function () {
+    $('.bloc11').css('display', 'block');
+    $('#masque10').css('display', 'block');
+    $('#recommencer').css('display', 'inline-block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+    $('#bonne').text(vraie);
+    $('#mauvaise').text(faux);
+})
+
+$('#choixJ2').click(function () {
+    $('.bloc11').css('display', 'block');
+    $('#masque10').css('display', 'block');
+    $('#recommencer').css('display', 'inline-block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+    $('#bonne').text(vraie);
+    $('#mauvaise').text(faux);
+})
+
+$('#choixJ3').click(function () {
+    $('.bloc11').css('display', 'block');
+    $('#masque10').css('display', 'block');
+    $('#recommencer').css('display', 'inline-block');
+    $(this).css('background-color', 'orange');
+    vraie += 1;
+    $('#bonne').text(vraie);
+    $('#mauvaise').text(faux);
+})
+
+$('#choixJ4').click(function () {
+    $('.bloc11').css('display', 'block');
+    $('#masque10').css('display', 'block');
+    $('#recommencer').css('display', 'inline-block');
+    $(this).css('background-color', 'orange');
+    faux += 1;
+    $('#bonne').text(vraie);
+    $('#mauvaise').text(faux);
+})
+
+$('#recommencer').click(function () {
+    $('.bloc1').css('display', 'none');
+    $('.bloc2').css('display', 'none');
+    $('.bloc3').css('display', 'none');
+    $('.bloc4').css('display', 'none');
+    $('.bloc5').css('display', 'none');
+    $('.bloc6').css('display', 'none');
+    $('.bloc7').css('display', 'none');
+    $('.bloc8').css('display', 'none');
+    $('.bloc9').css('display', 'none');
+    $('.bloc10').css('display', 'none');
+    $('.bloc11').css('display', 'none');
+    $('#masque1').css('display', 'none');
+    $('#masque2').css('display', 'none');
+    $('#masque3').css('display', 'none');
+    $('#masque4').css('display', 'none');
+    $('#masque5').css('display', 'none');
+    $('#masque6').css('display', 'none');
+    $('#masque7').css('display', 'none');
+    $('#masque8').css('display', 'none');
+    $('#masque9').css('display', 'none');
+    $('#masque10').css('display', 'none');
+    $('#recommencer').css('display', 'none');
+    $('#commencer').css('display', 'inline-block');
+    vraie = 0;
+    faux = 0;
+    $('#mauvaise').text("0");
+    $('#choixA1').css('background-color', 'white');
+    $('#choixA2').css('background-color', 'white');
+    $('#choixA3').css('background-color', 'white');
+    $('#choixA4').css('background-color', 'white');
+    $('#choixB1').css('background-color', 'white');
+    $('#choixB2').css('background-color', 'white');
+    $('#choixB3').css('background-color', 'white');
+    $('#choixB4').css('background-color', 'white');
+    $('#choixC1').css('background-color', 'white');
+    $('#choixC2').css('background-color', 'white');
+    $('#choixC3').css('background-color', 'white');
+    $('#choixC4').css('background-color', 'white');
+    $('#choixD1').css('background-color', 'white');
+    $('#choixD2').css('background-color', 'white');
+    $('#choixD3').css('background-color', 'white');
+    $('#choixD4').css('background-color', 'white');
+    $('#choixE1').css('background-color', 'white');
+    $('#choixE2').css('background-color', 'white');
+    $('#choixE3').css('background-color', 'white');
+    $('#choixE4').css('background-color', 'white');
+    $('#choixF1').css('background-color', 'white');
+    $('#choixF2').css('background-color', 'white');
+    $('#choixF3').css('background-color', 'white');
+    $('#choixF4').css('background-color', 'white');
+    $('#choixG1').css('background-color', 'white');
+    $('#choixG2').css('background-color', 'white');
+    $('#choixG3').css('background-color', 'white');
+    $('#choixG4').css('background-color', 'white');
+    $('#choixH1').css('background-color', 'white');
+    $('#choixH2').css('background-color', 'white');
+    $('#choixH3').css('background-color', 'white');
+    $('#choixH4').css('background-color', 'white');
+    $('#choixI1').css('background-color', 'white');
+    $('#choixI2').css('background-color', 'white');
+    $('#choixI3').css('background-color', 'white');
+    $('#choixI4').css('background-color', 'white');
+    $('#choixJ1').css('background-color', 'white');
+    $('#choixJ2').css('background-color', 'white');
+    $('#choixJ3').css('background-color', 'white');
+    $('#choixJ4').css('background-color', 'white');
+})
